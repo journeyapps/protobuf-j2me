1e72cabedd8e63c54a681c0ba9fd73092b295f78
Ralf Kistner <ralf.kistner@gmail.com>
2010-02-10 13:37:24 +0200

    Before changing lots of stuff

--- src/com/ponderingpanda/protobuf/CodedInputStream.java
+++ src/com/ponderingpanda/protobuf/CodedInputStream.java
@@ -4,6 +4,8 @@
  */
 package com.ponderingpanda.protobuf;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -12,56 +14,255 @@ import java.io.InputStream;
  * @author ralf
  */
 public class CodedInputStream {
+    private static final int DIRECT_READ_LIMIT = 20*1024;
     private InputStream in;
+
+    private int currentTag = 0;
 
     public CodedInputStream(InputStream in) {
         this.in = in;
+        advanceTag();
     }
 
-    public long readInt64(int field) {
-        if(!findTag(field))
+    public long readInt64(int field) throws IOException {
+        if(!skipTo(field, WireFormat.WIRETYPE_VARINT))
             return 0;
-        return 0;
+        long value = readInt64();
+        advanceTag();
+        return value;
     }
 
-    public int readInt32(int field) {
-        return 0;
+    public int readInt32(int field) throws IOException {
+        if(!skipTo(field, WireFormat.WIRETYPE_VARINT))
+            return 0;
+        int value = readInt32();
+        advanceTag();
+        return value;
     }
 
-    public long readUInt64(int field) {
-        return 0;
+    public long readUInt64(int field) throws IOException {
+        if(!skipTo(field, WireFormat.WIRETYPE_VARINT))
+            return 0;
+        long value = readUInt64();
+        advanceTag();
+        return value;
     }
 
-    public int readUInt32(int field) {
-        return 0;
+    public int readUInt32(int field) throws IOException {
+        if(!skipTo(field, WireFormat.WIRETYPE_VARINT))
+            return 0;
+        int value = readUInt32();
+        advanceTag();
+        return value;
     }
 
-    public long readSInt64(int field) {
-        return 0;
+    public long readSInt64(int field) throws IOException {
+        if(!skipTo(field, WireFormat.WIRETYPE_VARINT))
+            return 0;
+        long value = readSInt64();
+        advanceTag();
+        return value;
     }
 
-    public int readSInt32(int field) {
-        return 0;
+    public int readSInt32(int field) throws IOException {
+        if(!skipTo(field, WireFormat.WIRETYPE_VARINT))
+            return 0;
+        int value = readSInt32();
+        advanceTag();
+        return value;
     }
 
-    public float readFloat(int field) {
-        return 0;
+    public float readFloat(int field) throws IOException {
+        if(!skipTo(field, WireFormat.WIRETYPE_FIXED32))
+            return 0;
+        float value = readFloat();
+        advanceTag();
+        return value;
     }
 
-    public double readDouble(int field) {
-        return 0;
+    public double readDouble(int field) throws IOException {
+        if(!skipTo(field, WireFormat.WIRETYPE_FIXED64))
+            return 0;
+        double value = readDouble();
+        advanceTag();
+        return value;
     }
 
-    public String readString(int field) {
-        return "";
+    public String readString(int field) throws IOException {
+        if(!skipTo(field, WireFormat.WIRETYPE_LENGTH_DELIMITED))
+            return "";
+        String value = readString();
+        advanceTag();
+        return value;
     }
 
-    public byte[] readBytes(int field) {
-        return new byte[0];
+    public byte[] readBytes(int field) throws IOException {
+        if(!skipTo(field, WireFormat.WIRETYPE_LENGTH_DELIMITED))
+            return null;
+        byte[] value = readBytes();
+        advanceTag();
+        return value;
     }
 
-    public boolean findTag(int field) {
-        return false;
+    public boolean readMessage(int field, Message message) throws IOException {
+        //TODO: optimize
+        byte[] bytes = readBytes(field);
+        if(bytes == null)
+            return false;
+        ByteArrayInputStream bin = new ByteArrayInputStream(bytes);
+        CodedInputStream stream = new CodedInputStream(bin);
+        message.deserialize(stream);
+        return true;
+    }
+
+    public boolean hasVarInt(int field) throws IOException {
+        return skipTo(field, WireFormat.WIRETYPE_VARINT);
+    }
+
+    public boolean hasFixed32(int field) throws IOException {
+        return skipTo(field, WireFormat.WIRETYPE_FIXED32);
+    }
+
+    public boolean hasFixed64(int field) throws IOException {
+        return skipTo(field, WireFormat.WIRETYPE_FIXED64);
+    }
+
+    public boolean hasLengthDelimited(int field) throws IOException {
+        return skipTo(field, WireFormat.WIRETYPE_LENGTH_DELIMITED);
+    }
+
+    private boolean skipTo(int field, int wiretype) throws IOException {
+        while(true) {
+            if(currentTag == 0)
+                return false;
+            int cfield = WireFormat.getTagFieldNumber(currentTag);
+            if(cfield > field)
+                return false;
+            if(cfield == field && WireFormat.getTagWireType(currentTag) == wiretype)
+                return true;
+            skipTag(currentTag);
+            advanceTag();
+        }
+    }
+
+    private void advanceTag() {
+        try {
+            currentTag = readTag();
+        } catch(IOException e) {
+            currentTag = 0;
+        }
+    }
+
+	// -----------------------------------------------------------------
+
+	/** Read a {@code double} field value from the stream. */
+	private double readDouble() throws IOException {
+		return Double.longBitsToDouble(readRawLittleEndian64());
+	}
+
+	/** Read a {@code float} field value from the stream. */
+	private float readFloat() throws IOException {
+		return Float.intBitsToFloat(readRawLittleEndian32());
+	}
+
+	/** Read an {@code int64} field value from the stream. */
+	private long readInt64() throws IOException {
+		return readRawVarint64();
+	}
+
+    private long readUInt64() throws IOException {
+		return readRawVarint64();
+	}
+
+	/** Read an {@code int32} field value from the stream. */
+	private int readInt32() throws IOException {
+		return readRawVarint32();
+	}
+
+    /** Read an {@code uint32} field value from the stream. */
+	private int readUInt32() throws IOException {
+		return readRawVarint32();
+	}
+
+    private long readSInt64() throws IOException {
+        throw new UnsupportedOperationException("Not implemented yet");
+	}
+
+	/** Read an {@code int32} field value from the stream. */
+	private int readSInt32() throws IOException {
+		throw new UnsupportedOperationException("Not implemented yet");
+	}
+
+	/** Read a {@code bool} field value from the stream. */
+	private boolean readBool() throws IOException {
+		return readRawVarint32() != 0;
+	}
+
+	/** Read a {@code string} field value from the stream. */
+	private String readString() throws IOException {
+		byte[] bytes = readBytes();
+        return new String(bytes, "UTF-8");
+	}
+
+	/** Read a {@code bytes} field value from the stream. */
+	private byte[] readBytes() throws IOException {
+		int size = readRawVarint32();
+        int off = 0;
+        if(size < DIRECT_READ_LIMIT) {
+            // If the size is small enough, we read it directly
+            byte[] bytes = new byte[size];
+            //TODO: limit size
+            while(off < size) {
+                int read = in.read(bytes, off, size - off);
+                if(read < 0)
+                    throw new IOException("Unexpected end of stream");
+                off += read;
+            }
+            return bytes;
+        } else {
+            // If the size is large, we use a buffer, so we don't waste space
+            // if there isn't actually that much data
+            ByteArrayOutputStream out = new ByteArrayOutputStream(DIRECT_READ_LIMIT);
+            byte[] buffer = new byte[DIRECT_READ_LIMIT];
+            while(off < size) {
+                int read = in.read(buffer);
+                if(read < 0)
+                    throw new IOException("Unexpected end of stream");
+                off += read;
+                out.write(buffer, 0, read);
+            }
+            return out.toByteArray();
+        }
+	}
+
+    private int readTag() throws IOException {
+        int tag = readRawVarint32();
+        if(tag == 0)
+            throw new IOException("Invalid tag");
+        return tag;
+    }
+
+    private void skipTag(int tag) throws IOException {
+        if(tag == 0)
+            return;
+        final int dataType = WireFormat.getTagWireType(tag);
+		switch (dataType) {
+		case WireFormat.WIRETYPE_FIXED32:
+			readFloat();
+			break;
+		case WireFormat.WIRETYPE_FIXED64:
+			readDouble();
+			break;
+		case WireFormat.WIRETYPE_LENGTH_DELIMITED:
+            //TODO: skip the bytes instead of reading
+			readBytes();
+			break;
+		case WireFormat.WIRETYPE_VARINT:
+			readRawVarint64();
+			break;
+		default:
+			break;
+		}
     }
 
     /**
--- src/com/ponderingpanda/protobuf/TestMessage.java
+++ src/com/ponderingpanda/protobuf/TestMessage.java
@@ -14,7 +14,8 @@ import java.io.IOException;
 public class TestMessage implements Message {
     public float f = 0;
     public int i = 0;
-
+    private TestMessage nestedMessage;
+    
     public void serialize(CodedOutputStream out) throws IOException {
         out.writeInt32(3, i);
         out.writeFloat(5, f);
@@ -22,6 +23,9 @@ public class TestMessage implements Message {
 
     public void deserialize(CodedInputStream in) throws IOException {
         i = in.readInt32(3);
-        f = in.readFloat(5);
+        if(in.hasFixed32(5))
+            f = in.readFloat(5);
+        else
+            f = 123;
     }
 }
